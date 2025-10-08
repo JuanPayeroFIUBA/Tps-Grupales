3f0c9ca04abeee23275960a99b27ec7294b6314e
JuanPayeroFIUBA <jpayero@fi.uba.ar>
2025-10-08 18:59:19 -0300

    Add files via upload

--- diccionario/funciones_auxiliares.go
+++ diccionario/funciones_auxiliares.go
@@ -14,7 +14,7 @@ func fnvHash[K any](clave K, m uint64) uint64 {
 	return h.Sum64() % m
 }
 
-func buscarPosicionGuardar[K any, V comparable](clave K, hash HashCerrado[K, V]) (int, bool) {
+func hash_buscar[K any, V comparable](clave K, hash hashCerrado[K, V], queremos_guardar bool) (int, bool) {
 	ubicacion := int(fnvHash(clave, uint64(hash.capacidad)))
 	primera_borrada := -1
 	iteraciones := 0
@@ -26,12 +26,12 @@ func buscarPosicionGuardar[K any, V comparable](clave K, hash HashCerrado[K, V])
 			return ubicacion, true
 		}
 
-		if celda.estado == BORRADO && primera_borrada == -1 {
+		if queremos_guardar && celda.estado == BORRADO && primera_borrada == -1 {
 			primera_borrada = ubicacion
 		}
 
 		if celda.estado == VACIO {
-			if primera_borrada != -1 {
+			if queremos_guardar && primera_borrada != -1 {
 				return primera_borrada, false
 			}
 			return ubicacion, false
@@ -41,35 +41,13 @@ func buscarPosicionGuardar[K any, V comparable](clave K, hash HashCerrado[K, V])
 		iteraciones++
 	}
 
-	if primera_borrada != -1 {
+	if queremos_guardar && primera_borrada != -1 {
 		return primera_borrada, false
 	}
 	return ubicacion, false
 }
 
-func buscarClave[K any, V comparable](clave K, hash HashCerrado[K, V]) (int, bool) {
-	ubicacion := int(fnvHash(clave, uint64(hash.capacidad)))
-	iteraciones := 0
-
-	for iteraciones < hash.capacidad {
-		celda := hash.tabla[ubicacion]
-
-		if celda.estado == VACIO {
-			return -1, false
-		}
-
-		if celda.estado == OCUPADO && hash.funcion_igualdad_hash(celda.clave, clave) {
-			return ubicacion, true
-		}
-
-		ubicacion = (ubicacion + 1) % hash.capacidad
-		iteraciones++
-	}
-
-	return -1, false
-}
-
-func (hash *HashCerrado[K, V]) redimensionar(nueva_capacidad int) {
+func (hash *hashCerrado[K, V]) redimensionar(nueva_capacidad int) {
 	tabla_a_redimensionar := hash.tabla
 	tabla_redimensionada := make([]celdaHash[K, V], nueva_capacidad)
 	hash.tabla = tabla_redimensionada
@@ -84,7 +62,7 @@ func (hash *HashCerrado[K, V]) redimensionar(nueva_capacidad int) {
 	}
 }
 
-func (iter *IteradorDiccionario[K, V]) buscarProximaPosicionOcupada() {
+func (iter *iteradorDiccionario[K, V]) buscarProximaPosicionOcupada() {
 	for iter.posicion_actual < iter.capacidad {
 		if iter.tabla[iter.posicion_actual].estado == OCUPADO {
 			return
