2d67725833260c4e7bd5d648c685f5f85f283884
JuanPayeroFIUBA <jpayero@fi.uba.ar>
2025-10-12 16:39:59 -0300

    Update abb.go
    se cambia un poquito la funcion de borrar para que sea como las preguntas frecuentes de la consigna del tp

--- diccionario/abb.go
+++ diccionario/abb.go
@@ -82,20 +82,21 @@ func (abb *abb[K, V]) Borrar(clave K) V {
 
 	if nodo.izq != nil && nodo.der != nil {
 		menor_por_derecha, padre_menor_por_derecha := encontrarMenorPorDerecha(nodo.der, nodo_padre)
+		abb.Borrar(menor_por_derecha.clave)
 		nodo.clave = menor_por_derecha.clave
 		nodo.valor = menor_por_derecha.valor
 		//*menor_por_derecha, *nodo = *nodo, *menor_por_derecha
 		//borradoPreciso(padre, nodo, izquierda)
-		//abb.Borrar(menor_por_derecha.clave)
-		if nodo_padre != nil {
-			if padre_menor_por_derecha == nodo {
-				padre_menor_por_derecha.der = menor_por_derecha.der
-			} else {
-				padre_menor_por_derecha.izq = menor_por_derecha.der
-			}
-		} else {
-			nodo.der = menor_por_derecha.der
-		}
+
+		//if nodo_padre != nil {
+		//	if padre_menor_por_derecha == nodo {
+		//		padre_menor_por_derecha.der = menor_por_derecha.der
+		//	} else {
+		//		padre_menor_por_derecha.izq = menor_por_derecha.der
+		//	}
+		//} else {
+		//	nodo.der = menor_por_derecha.der
+		//}
 		abb.cantidad--
 		return valor_eliminado
 	}
