ff9792fd5e0b190d306351fcae6cb3417e45c6d9
Dylan Celentano <dyylancelentano@gmail.com>
2025-11-02 15:02:55 -0300

    codigo refactorizacion struct

--- algogram/algogram.go
+++ algogram/algogram.go
@@ -23,17 +23,12 @@ const (
 type usuario struct {
 	nombre            string
 	indice_en_arreglo int
-	feed              heap.ColaPrioridad[post_en_feed]
-}
-type post_en_feed struct {
-	mensaje                  string
-	id_post                  int
-	dif_indices_con_loggeado int
+	feed              heap.ColaPrioridad[*post]
 }
 
 type post struct {
 	id         int
-	publicador usuario
+	creador    *usuario
 	mensaje    string
 	likes      dicci.DiccionarioOrdenado[string, int]
 }
@@ -49,24 +44,27 @@ func comparacionStrings(a, b string) int {
 }
 
 func Setup(usuarios []string) {
-	hash_usuarios := dicci.CrearHash[string, int](igualdadStrings)                           //diccionario usuarios
-	hash_posts := dicci.CrearHash[int, dicci.DiccionarioOrdenado[string, int]](igualdadInts) //para likes
-	hash_feed := dicci.CrearHash[string, heap.ColaPrioridad[post]](igualdadStrings)          //para el feed
+	hash_usuarios := dicci.CrearHash[string, *usuario](igualdadStrings)
+	hash_posts := dicci.CrearHash[int, *post](igualdadInts)
 
-	var cuenta_logueada usuario
-
-	for indice, usuario := range usuarios {
-		hash_usuarios.Guardar(usuario, indice)
-		hash_feed.Guardar(usuario, heap.CrearHeap(func(post1, post2 post) int {
-			indice_usuario1 := int(math.Abs(float64(post1.publicador.indice_en_arreglo - indice)))
-			indice_usuario2 := int(math.Abs(float64(post2.publicador.indice_en_arreglo - indice)))
-			if indice_usuario1 == indice_usuario2 {
-				return -(post1.id - post2.id)
-			}
-			return -(indice_usuario1 - indice_usuario2)
-		}))
+	for indice, nombre_usuario := range usuarios {
+		u := &usuario{
+			nombre:            nombre_usuario,
+			indice_en_arreglo: indice,
+			feed: heap.CrearHeap(func(post1, post2 *post) int {
+				indice_usuario1 := int(math.Abs(float64(post1.creador.indice_en_arreglo - indice)))
+				indice_usuario2 := int(math.Abs(float64(post2.creador.indice_en_arreglo - indice)))
+				if indice_usuario1 == indice_usuario2 {
+					return -(post1.id - post2.id)
+				}
+				return -(indice_usuario1 - indice_usuario2)
+			}),
+		}
+		hash_usuarios.Guardar(nombre_usuario, u)
 	}
-	LeerEntradaComandos(hash_usuarios, hash_feed, hash_posts, cuenta_logueada)
+
+	var cuenta_logueada *usuario
+	LeerEntradaComandos(hash_usuarios, hash_posts, cuenta_logueada)
 }
 
 func LeerUsuariosDesdeArchivo(ruta string) []string {
@@ -87,7 +85,7 @@ func LeerUsuariosDesdeArchivo(ruta string) []string {
 	return usuarios
 }
 
-func LeerEntradaComandos(hash_users dicci.Diccionario[string, int], hash_feed dicci.Diccionario[string, heap.ColaPrioridad[post]], diccionario_posts dicci.Diccionario[int, dicci.DiccionarioOrdenado[string, int]], logueado usuario) {
+func LeerEntradaComandos(hash_users dicci.Diccionario[string, *usuario], diccionario_posts dicci.Diccionario[int, *post], logueado *usuario) {
 	entrada := bufio.NewScanner(os.Stdin)
 	for entrada.Scan() {
 		linea := strings.TrimSpace(entrada.Text())
@@ -103,13 +101,13 @@ func LeerEntradaComandos(hash_users dicci.Diccionario[string, int], hash_feed di
 
 		switch comando {
 		case constLogin:
-			Login(hash_users, &logueado, parametro)
+			logueado = Login(hash_users, logueado, parametro)
 		case constLogout:
-			Logout(&logueado)
+			logueado = Logout(logueado)
 		case constPublicar:
-			Publicar(diccionario_posts, hash_feed, &logueado, diccionario_posts.Cantidad(), parametro)
+			Publicar(hash_users, diccionario_posts, logueado, diccionario_posts.Cantidad(), parametro)
 		case constConstVerFeed:
-			VerProximoPostEnFeed(diccionario_posts, hash_feed, &logueado)
+			VerProximoPostEnFeed(logueado)
 		default:
 			id, err := strconv.Atoi(parametro)
 			if err != nil {
@@ -117,7 +115,7 @@ func LeerEntradaComandos(hash_users dicci.Diccionario[string, int], hash_feed di
 			}
 			switch comando {
 			case constDarLike:
-				LikearUnPost(diccionario_posts, &logueado, id)
+				LikearUnPost(diccionario_posts, logueado, id)
 			case constVerLikes:
 				VerLikesDeUnPost(diccionario_posts, id)
 
@@ -129,76 +127,83 @@ func LeerEntradaComandos(hash_users dicci.Diccionario[string, int], hash_feed di
 	}
 }
 
-func Login(hash_users dicci.Diccionario[string, int], logueado *usuario, usuario_a_loguear string) {
-	if logueado.nombre != "" {
+func Login(hash_users dicci.Diccionario[string, *usuario], logueado *usuario, usuario_a_loguear string) *usuario {
+	if logueado != nil {
 		fmt.Println("Error: Ya habia un usuario loggeado")
-		return
+		return logueado
 	}
 	if !hash_users.Pertenece(usuario_a_loguear) {
 		fmt.Println("Error: usuario no existente")
-		return
+		return nil
 	}
-	logueado.nombre = usuario_a_loguear
-	logueado.indice_en_arreglo = hash_users.Obtener(usuario_a_loguear)
+	logueado = hash_users.Obtener(usuario_a_loguear)
 	fmt.Println("Hola", usuario_a_loguear)
+	return logueado
 }
 
-func Logout(logueado *usuario) {
-	if logueado.nombre == "" {
+func Logout(logueado *usuario) *usuario {
+	if logueado == nil {
 		fmt.Println("Error: no habia usuario loggeado")
-		return
+		return nil
 	}
-	*logueado = usuario{}
 	fmt.Println("Adios")
+	return nil
 }
 
-func Publicar(diccionario_posts dicci.Diccionario[int, dicci.DiccionarioOrdenado[string, int]], hash_feed dicci.Diccionario[string, heap.ColaPrioridad[post]], logueado *usuario, ultimo_post_id int, mensaje string) {
-	if logueado.nombre == "" {
+func Publicar(hash_users dicci.Diccionario[string, *usuario], diccionario_posts dicci.Diccionario[int, *post], logueado *usuario, ultimo_post_id int, mensaje string) {
+	if logueado == nil {
 		fmt.Println("Error: no habia usuario loggeado")
 		return
 	}
-	diccionario_posts.Guardar(ultimo_post_id, dicci.CrearABB[string, int](comparacionStrings))
-	for iter := hash_feed.Iterador(); iter.HaySiguiente(); iter.Siguiente() {
-		usuario, cola := iter.VerActual()
-		if usuario != logueado.nombre {
-			cola.Encolar(post{mensaje: mensaje, id: ultimo_post_id, publicador: *logueado})
+
+	nuevo_post := &post{
+		id:         ultimo_post_id,
+		creador: logueado,
+		mensaje:    mensaje,
+		likes:      dicci.CrearABB[string, int](comparacionStrings),
+	}
+	diccionario_posts.Guardar(ultimo_post_id, nuevo_post)
+
+	// Agregar el post al feed de todos los usuarios excepto el creador
+	for iter := hash_users.Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+		nombre_usuario, usuario := iter.VerActual()
+		if nombre_usuario != logueado.nombre {
+			usuario.feed.Encolar(nuevo_post)
 		}
 	}
 	fmt.Println("Post publicado")
 }
 
-func VerProximoPostEnFeed(diccionario_posts dicci.Diccionario[int, dicci.DiccionarioOrdenado[string, int]], hash_feed dicci.Diccionario[string, heap.ColaPrioridad[post]], logueado *usuario) {
-	if logueado.nombre == "" || hash_feed.Obtener(logueado.nombre).EstaVacia() {
+func VerProximoPostEnFeed(logueado *usuario) {
+	if logueado == nil || logueado.feed.EstaVacia() {
 		fmt.Println("Usuario no loggeado o no hay mas posts para ver")
 		return
 	}
 
-	feed := hash_feed.Obtener(logueado.nombre)
-	p := feed.Desencolar()
-	likes := diccionario_posts.Obtener(p.id)
+	p := logueado.feed.Desencolar()
 	fmt.Printf("Post ID %d\n", p.id)
-	fmt.Printf("%s dijo: %s\n", p.publicador.nombre, p.mensaje)
-	fmt.Printf("Likes: %d\n", likes.Cantidad())
+	fmt.Printf("%s dijo: %s\n", p.creador.nombre, p.mensaje)
+	fmt.Printf("Likes: %d\n", p.likes.Cantidad())
 }
 
-func LikearUnPost(diccionario_posts dicci.Diccionario[int, dicci.DiccionarioOrdenado[string, int]], logueado *usuario, id int) {
-	if logueado.nombre == "" || !diccionario_posts.Pertenece(id) {
+func LikearUnPost(diccionario_posts dicci.Diccionario[int, *post], logueado *usuario, id int) {
+	if logueado == nil || !diccionario_posts.Pertenece(id) {
 		fmt.Println("Error: Usuario no loggeado o Post inexistente")
 		return
 	}
-	abb_likes := diccionario_posts.Obtener(id)
-	abb_likes.Guardar(logueado.nombre, 0)
+	p := diccionario_posts.Obtener(id)
+	p.likes.Guardar(logueado.nombre, 0)
 	fmt.Println("Post likeado")
 }
 
-func VerLikesDeUnPost(diccionario_posts dicci.Diccionario[int, dicci.DiccionarioOrdenado[string, int]], id int) {
-	if !diccionario_posts.Pertenece(id) || diccionario_posts.Obtener(id).Cantidad() == 0 {
+func VerLikesDeUnPost(diccionario_posts dicci.Diccionario[int, *post], id int) {
+	if !diccionario_posts.Pertenece(id) || diccionario_posts.Obtener(id).likes.Cantidad() == 0 {
 		fmt.Println("Error: Post inexistente o sin likes")
 		return
 	}
-	abb_likes := diccionario_posts.Obtener(id)
-	fmt.Printf("El post tiene %d likes:\n", abb_likes.Cantidad())
-	for iter := abb_likes.Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+	p := diccionario_posts.Obtener(id)
+	fmt.Printf("El post tiene %d likes:\n", p.likes.Cantidad())
+	for iter := p.likes.Iterador(); iter.HaySiguiente(); iter.Siguiente() {
 		nombre, _ := iter.VerActual()
 		fmt.Printf("\t%s\n", nombre)
 	}
